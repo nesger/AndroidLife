6b2ad7b3cc9372c600a417e4e60942d23d2fec44
nesger.zhan <nesger.zhan@dji.com>
2018-02-24 11:25:59 +0800

    引入滑动库，增加上下回弹，优化体验

--- app/src/main/java/com/nesger/recyclerview/base/BaseRecyclerViewActivity.java
+++ app/src/main/java/com/nesger/recyclerview/base/BaseRecyclerViewActivity.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import me.everything.android.ui.overscroll.OverScrollDecoratorHelper;
 
 public class BaseRecyclerViewActivity extends AppCompatActivity {
 
@@ -49,6 +50,8 @@ public class BaseRecyclerViewActivity extends AppCompatActivity {
         //设置适配器
         mAdapter = new BaseRecyclerViewAdapter(this, mItemList);
         mRecyclerView.setAdapter(mAdapter);
+        // Vertical
+        OverScrollDecoratorHelper.setUpOverScroll(mRecyclerView, OverScrollDecoratorHelper.ORIENTATION_VERTICAL);
     }
 
 }
